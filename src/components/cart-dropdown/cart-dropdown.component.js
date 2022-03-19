83d38d16ae59d6a16031827b57c63c3c741760ae
Srijita Thakur <srijita.thakur@gmail.com>
2022-03-20 00:46:47 +0530

    added onclickoutside custom hook

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -2,11 +2,15 @@ import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import { useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { useRef } from "react";
 import CartItem from "../cart-item/cart-item.component";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const CartDropdown = ({history,setHidden}) =>{
     const cart = useSelector(state=>state.cart);
-    return (<div className="cart-dropdown">
+    const ref = useRef();
+    useOnClickOutside(ref,()=>setHidden(true)) //custom hook
+    return (<div className="cart-dropdown" ref={ref}>
         <div className="cart-items">{
           cart.cartItems.length ? cart.cartItems.map((cartItem,idx)=><CartItem key={idx} item={cartItem}/>)
           :
